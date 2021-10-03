fa47c5502b4d6e52fb4f1c0b261d090784c16ccd
Raghad <Raghaddmufeed@hotmail.com>
2021-10-03 23:11:11 +0300

    Refactor code

--- javascript/issue.js
+++ javascript/issue.js
@@ -1,5 +1,3 @@
-id_list = [];
-
 class Issue {
     constructor(description, severity, assignedTo) {
         this.description = description;
@@ -10,10 +8,10 @@ class Issue {
         this.id = this.generateID();
     }
     generateID() {
-        let possibleIdCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const possibleIdCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let id = '';
         for (let i = 0; i < 5; i++) {
-            let size = Math.random() * 10 + 1;
+            const size = Math.random() * 10 + 1;
             for (let j = 0; j < size; j++) {
                 id += possibleIdCharacters.charAt(Math.floor(Math.random() * possibleIdCharacters.length));
             }
@@ -21,57 +19,56 @@ class Issue {
                 id += '-';
             }
         }
-        while (id_list.indexOf(id) !== -1) {
+        const issues = JSON.parse(window.localStorage.getItem('issuesList')) || [];
+        while (issues.find(issue => issue.id === id)) {
             id = this.generateID();
         }
-        id_list.push(id);
         return id;
     }
 };
 
 function changeBackgroundColor() {
-    let color = Math.floor(Math.random() * 16777215).toString(16);
+    const color = Math.floor(Math.random() * 16777215).toString(16);
     document.getElementById('background').style = 'background-color:#' + color + '80';
 }
 
 function setServerityinLocalSrorage(severity) {
-    window.localStorage.setItem('severity', severity);
+    severity !== 'null' ? window.localStorage.setItem('severity', severity) : window.localStorage.removeItem('severity');
     window.location.reload();
 }
 
 function checkSelectedSeverity(severity) {
-    let selectedSeverity = window.localStorage.getItem('severity');
+    const selectedSeverity = window.localStorage.getItem('severity');
     return severity === selectedSeverity ? 'selected' : '';
-
 }
 
 function setStatusinLocalSrorage(status) {
-    window.localStorage.setItem('status', status);
+    status !== 'null' ? window.localStorage.setItem('status', status) : window.localStorage.removeItem('status');
     window.location.reload();
 }
 
 function checkSelectedStatus(status) {
-    let selectedStatus = window.localStorage.getItem('status');
+    const selectedStatus = window.localStorage.getItem('status');
     return status === selectedStatus ? 'selected' : '';
 }
 
 function resetFilters() {
-    window.localStorage.setItem('severity', 'null');
-    window.localStorage.setItem('status', 'null');
+    window.localStorage.removeItem('severity');
+    window.localStorage.removeItem('status');
     window.location.reload();
 }
 
 function setAddIssueDisabled() {
-    let description = document.getElementById('description');
-    let severity = document.getElementById('severity');
-    let assignedTo = document.getElementById('assignedTo');
+    const description = document.getElementById('description');
+    const severity = document.getElementById('severity');
+    const assignedTo = document.getElementById('assignedTo');
     if (description.value.trim() === '' ||
         assignedTo.value.trim() === '') {
         document.getElementById('addButton').disabled = true;
         return;
     }
-    let issues = JSON.parse(window.localStorage.getItem('issues_list')) || [];
-    let foundElement = issues.find(issue =>
+    const issues = JSON.parse(window.localStorage.getItem('issueList')) || [];
+    const foundElement = issues.find(issue =>
         issue.description === description.value &&
         issue.severity === severity.value &&
         issue.assignedTo === assignedTo.value
@@ -85,56 +82,40 @@ function setAddIssueDisabled() {
 }
 
 function severityFilter(issues) {
-    let severity = window.localStorage.getItem('severity');
-    if (severity !== null && severity !== 'null') {
-        return issues.filter(issue => {
-            return issue.severity === severity;
-        })
-    }
-    return issues;
+    const severity = window.localStorage.getItem('severity');
+    return severity ? issues.filter(issue => issue.severity === severity) : issues;
 }
 
 function statusFilter(issues) {
-    let status = window.localStorage.getItem('status');
-    if (status !== null && status !== 'null') {
-        return issues.filter(function(issue) {
-            return issue.status === status;
-        })
-    }
-    return issues;
+    const status = window.localStorage.getItem('status');
+    return status ? issues.filter(issue => issue.status === status) : issues;
 }
 
 function addIssue() {
-    let issue = new Issue(document.getElementById('description').value,
+    const issue = new Issue(document.getElementById('description').value,
         document.getElementById('severity').value, document.getElementById('assignedTo').value);
-    let issues = JSON.parse(window.localStorage.getItem('issues_list')) || [];
+    let issues = JSON.parse(window.localStorage.getItem('issuesList')) || [];
     issues.push(issue);
-    window.localStorage.setItem('issues_list', JSON.stringify(issues));
+    window.localStorage.setItem('issuesList', JSON.stringify(issues));
     window.location.reload();
 }
 
 function orderIssues() {
-    var issues = JSON.parse(window.localStorage.getItem('issues_list'));
-    var lowIssues = issues.filter(function(issue) {
-        return issue.severity === 'Low'
-    });
-    var mediumIssues = issues.filter(function(issue) {
-        return issue.severity === 'Medium'
-    });
-    var highIssues = issues.filter(function(issue) {
-        return issue.severity === 'High'
-    });
+    let issues = JSON.parse(window.localStorage.getItem('issuesList'));
+    const lowIssues = issues.filter(issue => issue.severity === 'Low');
+    const mediumIssues = issues.filter(issue => issue.severity === 'Medium');
+    const highIssues = issues.filter(issue => issue.severity === 'High');
     issues = [...lowIssues, ...mediumIssues];
     issues = [...issues, ...highIssues];
-    window.localStorage.setItem('issues_list', JSON.stringify(issues));
+    window.localStorage.setItem('issuesList', JSON.stringify(issues));
     window.location.reload();
 }
 
 function closeIssue(id) {
-    let issues = JSON.parse(window.localStorage.getItem('issues_list'));
+    let issues = JSON.parse(window.localStorage.getItem('issuesList'));
     let issue = issues.find(issue => issue.id === id);
     issue.status = 'closed';
-    window.localStorage.setItem('issues_list', JSON.stringify(issues));
+    window.localStorage.setItem('issuesList', JSON.stringify(issues));
     window.location.reload();
 }
 
@@ -145,10 +126,8 @@ function openDeleteDialog(id) {
 }
 
 function deleteIssue(id) {
-    let issues = JSON.parse(window.localStorage.getItem('issues_list'));
-    issues = issues.filter(issue => {
-        return issue.id !== id
-    })
-    window.localStorage.setItem('issues_list', JSON.stringify(issues));
+    let issues = JSON.parse(window.localStorage.getItem('issuesList'));
+    issues = issues.filter(issue => issue.id !== id);
+    window.localStorage.setItem('issuesList', JSON.stringify(issues));
     window.location.reload();
 }
